95ffd23400907de7e3326377130186c55bb4361c
Panchorn Lertvipada <panchorn.ler@ascendcorp.com>
2020-03-22 23:19:52 +0700

    Add function trigger to build job with parameters

--- src/JenkinsLibrary/jenkins_face.py
+++ src/JenkinsLibrary/jenkins_face.py
@@ -1,9 +1,11 @@
 import requests
-import urllib3
 import json
+import urllib3
 
 BASE_ENDPOINT = '{}://{}:{}@{}/'
 GET_JOB = '{}/api/json'
+GET_JOB_BUILD = '{}/{}/api/json'
+BUILD_JOB_WITH_PARAMETERS = '{}/buildWithParameters'
 
 
 class JenkinsFace(object):
@@ -24,6 +26,8 @@ class JenkinsFace(object):
         self._settings['allow_redirects'] = False
 
     def get_job(self, name=None):
+        if not name:
+            raise Exception('Job name should not be None')
         req = self._session.prepare_request(
             requests.Request(
                 'GET',
@@ -33,6 +37,21 @@ class JenkinsFace(object):
         return self._get_response(
             self._send(req)
         )
+
+    def build_with_parameters(self, name=None, data=None):
+        if not name:
+            raise Exception('Job name should not be None')
+        job_detail = self.get_job(name)
+        req = self._session.prepare_request(
+            requests.Request(
+                'POST',
+                self._job_url(BUILD_JOB_WITH_PARAMETERS, name),
+                data=data
+            )
+        )
+        response = self._send(req)
+        response.raise_for_status()
+        return job_detail['nextBuildNumber']
 
     def _send(self, req):
         return self._session.send(req, **self._settings)
