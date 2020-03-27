0fc84c99a28345b2e36f6883db165dd467c70fc8
Authapon Kongkaew <ohmrefresh@gmail.com>
2020-03-27 15:41:32 +0700

    add generate doc

---
+++ generate.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from os.path import join, dirname
+from robot.libdoc import libdoc
+
+def main():
+    libdoc(join(dirname(__file__), 'JenkinsLibrary'), join(dirname(__file__), 'docs', 'JenkinsLibrary.html'))
+
+
+if __name__ == '__main__':
+    main()
