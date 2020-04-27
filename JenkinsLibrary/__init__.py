1c14fe9b7701bc4fc51a6fd8f3e4374b1c600dcc
Panchorn <nonpcn@gmail.com>
2020-04-28 00:02:20 +0700

    Add docs

--- JenkinsLibrary/__init__.py
+++ JenkinsLibrary/__init__.py
@@ -7,6 +7,16 @@ __version__ = VERSION
 class JenkinsLibrary(JenkinsFace):
     """JenkinsLibrary is a robotframework library for wrapping jenkins api
 
+    == Example Test Cases ==
+    | *** Settings ***       |
+    | Library                | JenkinsLibrary   |
+    |                        |
+    | *** Test Cases ***     |
+    | create session jenkins | ${protocol}      | ${host}          | ${username}      | ${password}      |
+    | ${job_details}=        | Get Jenkins Job  | ${job_full_name} |
+    | ${job_build_details}=  | Get Jenkins Job Build | ${job_full_name} | ${build_number}  |
+    | ${build_number}=       | Build Jenkins With Parameters | ${job_full_name} | ${parameters_string} |
+
     """
 
     ROBOT_LIBRARY_SCOPE = "GLOBAL"
